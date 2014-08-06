d86a16488cd04882de201b87643fe616b0cb878b
kikrahau <vincent.koch@hotmail.com>
2014-08-06 17:28:17 +0100

    finish all exercises of version 6

--- directory.rb
+++ directory.rb
@@ -7,19 +7,17 @@ end
 
 def get_info(info)
 	puts "Please enter the next students #{info}"
-	information=gets.chomp
+	information=gets.chop
 	info=information.empty? ? "N/A" : information
 	return info
 end
 
-
 def input_students
-	puts "Please enter a name of a student and press enter"
-	puts "In case you are done entering, leave all the spaces empty and press enter"
+	puts "Please enter a name,cohort and age of a student and press enter"
+	puts "In case you are done entering, please type exit and press enter"
 	puts "--------------"
 	#create an empty array
 	students = []
-	#while the name is not empty, repeat this code
 	while true
 		name=get_info("name")
 		break if name == "exit"
@@ -28,20 +26,37 @@ def input_students
 		age=get_info("age")
 		break if age == "exit"
 		students << {:name => name, :cohort => cohort, :age => age}
+		if students.length > 1
+		puts "Now we have #{students.length} students."
+		else
+		puts "Now we have #{students.length} student."
+		end
 	end
 	#return the array of students
 	students
 end
 
-def printing(students)
-	index=0
-	until index == students.length do
-		student = students[index]
-		puts "#{index + 1}. Name:#{student[:name]} Age: #{student[:age]} Cohort:#{student[:cohort]} cohort)"
-		index += 1
+def get_cohort_list(students)
+	students.map {|student| student[:cohort]}.uniq
+end
+
+def print_by_cohort(students)
+	cohort_list=get_cohort_list(students)
+	cohort_list.each do |cohort|
+		puts "All Students in the #{cohort} cohort:"
+		students.each_with_index do |student,index|
+			if student[:cohort] == cohort
+				printing(student,index)
+			end
+		end
 	end
+end
+
+def printing(student, index)
+	puts "#{index + 1}. Name:#{student[:name]} Age: #{student[:age]} Cohort:#{student[:cohort]}"
 end
 #call the methods
 students = input_students
 print_header
-printing(students)
+#printing(students)
+print_by_cohort(students)
