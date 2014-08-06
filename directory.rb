1fee2670d1de180ff2342e85206629839bda2fa0
kikrahau <vincent.koch@hotmail.com>
2014-08-06 10:58:46 +0100

    create array

--- directory.rb
+++ directory.rb
@@ -1,16 +1,26 @@
 #!/usr/bin/env ruby
+student_count = 10
+#create array with students
+students = [
+	"Vincent Koch", 
+	"Harry Eiertyp", 
+	"Peter Willi", 
+	"Franz Gerry",
+	"William Heiopei",
+	"Singh Gun Gap",
+	"Xi Mei Dong",
+	"Shing Shang Shong"
+]
+
+#print the array
 puts "The students of my cohort at Makers Academy"
 puts "--------------"
-puts "Vincent Koch"
-puts "Harry Eiertyp"
-puts "Vincent Koch"
-puts "Harry Eiertyp"
-puts "Vincent Koch"
-puts "Harry Eiertyp"
-puts "Vincent Koch"
-puts "Harry Eiertyp"
-puts "Vincent Koch"
-puts "Harry Eiert"
-print "Overall, we have "
-print 10
-puts " great students"
+puts students [0]
+puts students [1]
+puts students [2]
+puts students [3]
+puts students [4]
+puts students [5]
+puts students [6]
+puts students [7]
+print "Overall, we have #{student_count} great students"
