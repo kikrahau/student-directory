a8e6831d11fbda406a4684c0806e1f7144b07b8d
kikrahau <vincent.koch@hotmail.com>
2014-08-06 12:09:55 +0100

    final status of challenge

--- directory.rb
+++ directory.rb
@@ -1,31 +1,36 @@
 #!/usr/bin/env ruby
-#create array with students
-students = [
-	{:name => "Vincent Koch", :cohort => :august},
-	{:name => "Harry Eiertyp", :cohort => :august},
-	{:name => "Peter Willi", :cohort => :august},
-	{:name => "Franz Gerry", :cohort => :august},
-	{:name => "William Heiopei", :cohort => :august},
-	{:name => "Singh Gun Gap", :cohort => :august},
-	{:name => "Xi Mei Dong", :cohort => :august},
-	{:name => "Shing Shang Shong", :cohort => :august}
-]
 #print the header with the instructions
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "--------------"
 end
-#print 
-def print(students)
-	students.each do |student|
-	puts "#{student[:name]} (#{student[:cohort]})"
+
+def input_students
+	puts "Please enter a name of a student and press enter"
+	puts "In case you are done entering names, press enter twice"
+	puts "--------------"
+	#create an empty array
+	students = []
+	#get the first name
+	name = gets.chomp
+	#while the name is not empty, repeat this code
+	while !name.empty? do 
+		#add the student hash to the array
+		students << {:name => name, :cohort => :august}
+		puts "Now we have #{students.length} students"
+		#get another name from the user
+		name=gets.chomp
 	end
+	#return the array of students
+	students
 end
 
-def print_footer(names)
-	puts "Overall, we have #{names.length} great students"
+def print(students)
+	students.each do |student|
+		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+	end
 end
 #call the methods
+students = input_students
 print_header
 print(students)
-print_footer(students)
